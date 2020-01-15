4e174e206895160b6547fb2b9d1cbd62beb65382
William Lin <willlin1996@gmail.com>
2020-01-15 19:42:43 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,6 +3,7 @@ board = [" "," "," "," "," "," "," "," "," "]
 index = 
 def position_taken?(board, index)
   if board[index] == "X"
+    print "true"
     elsif board[index] == "O"
     print "true"
   else
