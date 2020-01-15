d298fea73a2b31f3292b92bf14dd19adef291c6d
William Lin <willlin1996@gmail.com>
2020-01-15 19:36:36 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,11 @@
 # code your #position_taken? method here!
+board = [" "," "," "," "," "," "," "," "," "]
+index = 
+def position_taken?(board, index)
+  if board[index] == "X"
+    elsif board[index] == "O"
+    print "true"
+  else
+    print "false"
+end
+end
